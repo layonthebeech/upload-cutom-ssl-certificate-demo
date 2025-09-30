58edd0a9500eedc7cd3252a348c7abb9704a5c02
layonthebeech <beecherp@gmail.com>
2025-09-30 15:09:50 -0700

    null check env vars

--- upload.js
+++ upload.js
@@ -7,6 +7,14 @@ const VERCEL_API_TOKEN = process.env.VERCEL_API_TOKEN;
 const TEAM_ID = process.env.VERCEL_TEAM_ID;
 
 export async function uploadCert({ cert, key, ca }) {
+  if (!VERCEL_API_TOKEN) {
+    throw new Error("Missing VERCEL_API_TOKEN environment variable");
+  }
+
+  if (!TEAM_ID) {
+    throw new Error("Missing VERCEL_TEAM_ID environment variable");
+  }
+
   const qs = new URLSearchParams({ teamId: TEAM_ID });
   const url = `https://api.vercel.com/v8/certs?${qs.toString()}`;
 
